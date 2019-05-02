026efeda89337fcad1e87cd8562e8860b4f0c773
pasin notebook <pasin.sjr@gmail.com>
2019-05-02 10:51:19 +0700

    change return TypeError in common class to throw

--- libs/common-classes/src/index.ts
+++ libs/common-classes/src/index.ts
@@ -2,9 +2,10 @@ class String40 {
   private constructor(public value: string) {}
 
   public static create(value: string) {
-    return value.length > 40
-      ? new TypeError('String must has less than or equal 40 characters')
-      : new String40(value);
+    if (value.length > 40) {
+      throw new TypeError('String must has less than or equal 40 characters');
+    }
+    return new String40(value);
   }
 }
 
