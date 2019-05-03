d7e37956cf7e31126f3bfabbf967c699e2b9d5d8
pasin notebook <pasin.sjr@gmail.com>
2019-05-03 14:00:30 +0700

    update card-slider style

--- libs/shared-ui/src/lib/card-slider/card-slider.component.ts
+++ libs/shared-ui/src/lib/card-slider/card-slider.component.ts
@@ -2,13 +2,16 @@ import {
   Component,
   OnInit,
   Input,
-  ChangeDetectionStrategy
+  ChangeDetectionStrategy,
+  Renderer2,
+  ElementRef
 } from '@angular/core';
 import {
   String20,
   StringPath,
   StringURL
 } from '@nx-angular-resume/common-classes';
+import { CdkDragEnd } from '@angular/cdk/drag-drop';
 
 interface CardElement {
   title: String20;
@@ -25,7 +28,19 @@ interface CardElement {
 export class CardSliderComponent implements OnInit {
   @Input() cards: CardElement[];
 
-  constructor() {}
+  constructor(private renderrer: Renderer2) {}
 
   ngOnInit() {}
+
+  onDragEnded(event: CdkDragEnd) {
+    const source: any = event.source;
+    event.source.reset();
+    this.renderrer.addClass(source.element.nativeElement, 'transition-move');
+    setTimeout(() => {
+      this.renderrer.removeClass(
+        event.source.element.nativeElement,
+        'transition-move'
+      );
+    }, 500);
+  }
 }
