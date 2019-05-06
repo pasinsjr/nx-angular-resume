0fbd6612c453144bb67d4701104ae390d7256480
pasin notebook <pasin.sjr@gmail.com>
2019-05-06 13:10:42 +0700

    add PWA

--- apps/nx-angular-resume/src/app/app.module.ts
+++ apps/nx-angular-resume/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
