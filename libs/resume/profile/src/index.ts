8a979ff1e577aa8f9edd1c2f858f94ce35dafba1
pasin notebook <pasin.sjr@gmail.com>
2019-05-06 11:39:29 +0700

    delete profile component

--- libs/resume/profile/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/+state/profile.facade';
-export * from './lib/+state/profile.reducer';
-export * from './lib/+state/profile.selectors';
-export * from './lib/resume-profile.module';
