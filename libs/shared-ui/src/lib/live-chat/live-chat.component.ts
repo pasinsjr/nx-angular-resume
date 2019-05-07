676d28c1437d56ee48c82ec09de4d1a9ead5b81b
pasin notebook <pasin.sjr@gmail.com>
2019-05-07 11:24:10 +0700

    add live-chat ui

---
+++ libs/shared-ui/src/lib/live-chat/live-chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nx-angular-resume-live-chat',
+  templateUrl: './live-chat.component.html',
+  styleUrls: ['./live-chat.component.scss']
+})
+export class LiveChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
