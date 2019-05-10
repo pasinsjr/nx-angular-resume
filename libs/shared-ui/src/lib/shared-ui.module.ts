7670819ffce4cad6dab717543ba8d9071e213a32
pasin notebook <pasin.sjr@gmail.com>
2019-05-10 14:31:31 +0700

    import forms module in shared-ui

--- libs/shared-ui/src/lib/shared-ui.module.ts
+++ libs/shared-ui/src/lib/shared-ui.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { TimelineComponent } from './timeline/timeline.component';
 import { CardSliderComponent } from './card-slider/card-slider.component';
 import { LiveChatComponent } from './live-chat/live-chat.component';
 @NgModule({
-  imports: [CommonModule, DragDropModule],
+  imports: [CommonModule, FormsModule, DragDropModule],
   declarations: [TimelineComponent, CardSliderComponent, LiveChatComponent],
   exports: [TimelineComponent, CardSliderComponent, LiveChatComponent]
 })
