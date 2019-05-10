f15476b13649993d70cd420f5ae22c1947bc3362
pasin notebook <pasin.sjr@gmail.com>
2019-05-10 14:33:02 +0700

    update live-chat ui

--- libs/shared-ui/src/lib/live-chat/live-chat.component.ts
+++ libs/shared-ui/src/lib/live-chat/live-chat.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+  ElementRef,
+  ViewChild,
+  AfterViewChecked
+} from '@angular/core';
+import { String150 } from '@nx-angular-resume/common-classes';
+import { SendMessage } from 'libs/live-chat/src/lib/+state/live-chat.actions';
 
+interface Message {
+  timeStamp: Date;
+  description: String150;
+}
 @Component({
   selector: 'nx-angular-resume-live-chat',
   templateUrl: './live-chat.component.html',
-  styleUrls: ['./live-chat.component.scss']
+  styleUrls: ['./live-chat.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LiveChatComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class LiveChatComponent implements OnInit, AfterViewChecked {
+  @Input()
+  set messages(messages: Message[]) {
+    this._messages = messages;
+    this.neeedToScroll = true;
   }
 
+  @Input() anonymousMode: boolean;
+
+  @Output() sendMessage: EventEmitter<String150> = new EventEmitter();
+  @Output() linkAccount: EventEmitter<void> = new EventEmitter();
+
+  @ViewChild('chatBox')
+  private chatBox: ElementRef;
+
+  private neeedToScroll: boolean;
+
+  _messages: Message[];
+  testInput: string;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngAfterViewChecked() {
+    if (this.neeedToScroll) {
+      this.chatBox.nativeElement.scrollTop = this.chatBox.nativeElement.scrollHeight;
+      this.neeedToScroll = false;
+    }
+  }
+
+  emitMessage(): void {
+    this.sendMessage.emit(String150.create(this.testInput));
+    this.testInput = '';
+  }
+
+  emitLinkAccount(): void {
+    this.linkAccount.emit();
+  }
 }
