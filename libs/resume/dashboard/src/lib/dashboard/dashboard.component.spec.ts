a1570d76d5f7ea2fa4315f628e0ba6f4f175def2
pasin notebook <pasin.sjr@gmail.com>
2019-05-20 14:44:25 +0700

    fixed dash-board component test

--- libs/resume/dashboard/src/lib/dashboard/dashboard.component.spec.ts
+++ libs/resume/dashboard/src/lib/dashboard/dashboard.component.spec.ts
@@ -1,25 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { AuthFacade, AuthModule } from '@nx-angular-resume/auth';
+import { UserFacade, UserModule } from '@nx-angular-resume/user';
+import { LiveChatModule } from '@nx-angular-resume/live-chat';
+import { ResumeUiModule } from '@nx-angular-resume/resume-ui';
+import { SharedUiModule } from '@nx-angular-resume/shared-ui';
+import { LiveChatContainerComponent } from '../live-chat-container/live-chat-container.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
-  }));
+  const authFacadeMock = {
+    loadAuth: jest.fn()
+  } as any;
+  const userFacadeMock = {
+    loadAll: jest.fn()
+  } as any;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new DashboardComponent(authFacadeMock, userFacadeMock);
+    component.ngOnInit();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be called loadAll', () => {
+    expect(authFacadeMock.loadAuth).toHaveBeenCalled();
+    expect(userFacadeMock.loadAll).toHaveBeenCalled();
   });
 });
